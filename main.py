60f7542f4f021f9cdc09c116f93b5f2ee17e16af
Dan23022 <d.s.23.shore@gmail.com>
2023-04-29 13:59:18 +0100

    baseline

--- main.py
+++ main.py
@@ -16,10 +16,19 @@ class Player(pygame.sprite.Sprite):
         self.images.append(sprite)
         self.images.append(pygame.transform.flip(sprite, flip_x=True, flip_y=False))
         self.image = self.images[0]
-        self.speed = 14  # Define the player's speed
+        self.speed = 5  # Define the player's speed
         self.rect = self.image.get_rect()
 
-    def update(self, keys):
+        self.max_health = 100
+        self.health = self.max_health
+
+        self.health_bar_height = 40
+        self.health_bar_colour = (255, 0, 0)
+
+        self.attack_cooldown = 10
+        self.last_attack_time = 0
+
+    def update(self, keys, dt):
 
         if keys[pygame.K_d]:
             if player.rect.x < screen.get_width() - player.rect.width:
@@ -39,6 +48,31 @@ class Player(pygame.sprite.Sprite):
             if player.rect.y < screen.get_height() - player.image.get_height():
                 self.rect.y += self.speed
 
+    def attack(self):
+        current_time = pygame.time.get_ticks() / 1000
+
+        if current_time - self.last_attack_time >= self.attack_cooldown:
+            self.last_attack_time = current_time
+
+            zombies_to_kill = []
+            for zombie in zombie_list:
+                if abs(self.rect.centerx - zombie.rect.centerx) <= 20 and abs(
+                        self.rect.centery - zombie.rect.centery) <= 20:
+                    zombies_to_kill.append(zombie)
+
+            for zombie in zombies_to_kill:
+                zombie_list.remove(zombie)
+
+    def draw_health_bar(self):
+        health_bar_width = int((self.health / self.max_health) * screen.get_width())
+        health_bar_rect = pygame.Rect(0, 0, health_bar_width, self.health_bar_height)
+        pygame.draw.rect(screen, self.health_bar_colour, health_bar_rect)
+
+    def handle_collision(self, collided_zombies):
+        self.health -= len(collided_zombies)
+        if self.health <= 0:
+            pass
+
 class Zombie(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
@@ -52,17 +86,23 @@ class Zombie(pygame.sprite.Sprite):
         self.speed = 2  # Define the player's speed
         self.rect = self.image.get_rect()
 
+        self.alive = True
+
     def update(self, player):
-        dx = player.rect.centerx - self.rect.centerx
-        dy = player.rect.centery - self.rect.centery
-        dist = max(1, abs(dx) + abs(dy))
-        self.rect.x += (dx / dist) * self.speed
-        self.rect.y += (dy / dist) * self.speed
+        if self.alive:
+            if self.rect.x < player.rect.x:
+                self.rect.x += self.speed
+            elif self.rect.x > player.rect.x:
+                self.rect.x -= self.speed
+            if self.rect.y < player.rect.y:
+                self.rect.y += self.speed
+            elif self.rect.y > player.rect.y:
+                self.rect.y -= self.speed
 
 
 player = Player()
-player.rect.x = 0
-player.rect.y = 0
+player.rect.x = screen.get_width() // 2
+player.rect.y = screen.get_height() // 2
 player_list = pygame.sprite.Group()
 player_list.add(player)
 
@@ -83,6 +123,9 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE:
+                player.attack()
 
     keys = pygame.key.get_pressed()  # Check the state of all keys
 
@@ -91,7 +134,7 @@ while running:
 
     screen.blit(background_surface, (0, 0))
 
-    player_list.update(keys)  # Update the player's position with the current keys
+    player_list.update(keys, dt)  # Update the player's position with the current keys
     player_list.draw(screen)
 
     for zombie in zombie_list:
@@ -99,8 +142,10 @@ while running:
 
     zombie_list.draw(screen)
 
-    if len(zombie_list) < 20:
-        if random.random() < 0.01:  # Adjust the probability of spawning a new zombie
+    player.draw_health_bar()
+
+    if len(zombie_list) < 75:
+        if random.random() < 0.1:  # Adjust the probability of spawning a new zombie
             zombie = Zombie()
             side = random.randint(1, 4)  # Determine the side from which the zombie will spawn
 
@@ -118,6 +163,11 @@ while running:
                 zombie.rect.y = random.randint(screen.get_height() + 80, screen.get_height() + 100)
 
             zombie_list.add(zombie)
+
+    zombie_hit = pygame.sprite.spritecollide(player, zombie_list, False)
+
+    if zombie_hit:
+        player.handle_collision(zombie_hit)
 
     pygame.display.flip()
 
